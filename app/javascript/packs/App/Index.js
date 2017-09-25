d1d2db208ac35a4e3bab64dd32d73657ead7de2a
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-09-25 22:05:07 +0100

    Project setup

---
+++ app/javascript/packs/App/Index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+class Index extends React.Component {
+
+    constructor(props) {
+	super(props);
+    }
+
+    render() {
+	return (
+	    <div>
+		<p> hello from Ives </p>
+	    </div>
+	);
+    }
+
+}
+
+export default Index;
+
