1263fbc027939eda35f688cb95047d4c0c69d902
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-09-27 03:52:11 +0100

    Prepare Devise for json authentication
    I also remove the sign_in and sign_out alert from devise to
    avoid its automatic injection of flash messages after a user signs_in
    and signs_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  #devise_for :users 
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'} 
+  resources :reviews
+  resources :home
+  resources :stores
 end
