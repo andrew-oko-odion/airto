5e2d9ca03f41ddbf8eebe6c76a872acce3ae7ae8
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-10-22 18:09:40 +0100

    Fixed semantic-ui install problems on heroku

---
+++ app/assets/javascripts/api.js
@@ -0,0 +1,10 @@
+
+
+/* Semantic-ui API module here */
+
+$.fn.api.settings.api = {
+    'signin user'   : '/user.json',
+    'signout user'  : '/users/delete',
+    'create user'   : '/create',
+    'search'        : '/search/?query={value}'
+};
--- app/javascript/packs/api.js
+++ app/javascript/packs/api.js
@@ -1,11 +1,10 @@
 
 
 /* Semantic-ui API module here */
-
-
 $.fn.api.settings.api = {
-    'signin user'   : '/user.json',
-    'signout user'  : '/users/delete',
-    'create user'   : '/create',
+    'signin user'   : '/users/sign_in.json',
+    'signout user'  : '/users/sign_out.json',
+    'create user'   : '/users.json',
     'search'        : '/search/?query={value}'
 };
+
